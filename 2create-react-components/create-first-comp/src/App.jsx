08b2cb2332c38dadb8b6eab73d9de3d9be5fe40f
1amsahil <sahil981156@gmail.com>
2025-03-06 13:32:41 +0530

    2 Basic React Components

---
+++ 2create-react-components/create-first-comp/src/App.jsx
@@ -0,0 +1,13 @@
+import Title from "./Title"
+import Hello from "./Hello"
+import './App.css'
+
+function App() {
+  return <>
+    <Title className = "title"/>
+    <Hello/>
+    <button>click me</button>
+  </>
+}
+
+export default App
