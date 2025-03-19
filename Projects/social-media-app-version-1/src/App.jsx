65d8094d25b33fae4aa8383d59cc282d2c733c56
1amsahil <sahil981156@gmail.com>
2025-03-20 00:18:12 +0530

    UI (By BootStrap) Completed

--- Projects/social-media-app-version-1/src/App.jsx
+++ Projects/social-media-app-version-1/src/App.jsx
@@ -1,11 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Sidebar from "./components/Sidebar/Sidebar"
+import CreatePost from "./components/CreatePost/CreatePost"
+import Post from "./components/Post/Post"
+import PostList from "./components/Post/PostList"
+
+import "./App.css"
 
 function App() {
 
-  return (
-    <>
+  return <div className="app-container">
 
-    </>
-  )
+        <Sidebar/> 
+        <div className="content" >  
+          <Header/>
+          <CreatePost/>
+          <Post/>
+          <Footer/>
+        </div>       
+    </div>
 }
 
 export default App
