28540e9e3df87be9bdfe50e1b5ff63a8b53ffe66
1amsahil <sahil981156@gmail.com>
2025-03-24 00:32:16 +0530

    Loading Spinner Added

--- 11Hooks/useeffect-hooks/src/components/Post/PostList.jsx
+++ 11Hooks/useeffect-hooks/src/components/Post/PostList.jsx
@@ -1,30 +1,35 @@
 import { ContextPostList } from "../../store/Post-list-store";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import WelcomeMessage from "../WelcomeMessage/WelcomeMessage";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 
 export default function PostList() {
 
   const {postData,addInitialPosts} = useContext(ContextPostList);
+  const [fetching, setFetching] = useState(false);
 
+  // useEffect
   useEffect(() => {
+
+    setFetching(true);
     
     fetch("https://dummyjson.com/posts")
     .then( res => res.json() )
     .then( (data) => {
-      addInitialPosts(data.posts)
+      addInitialPosts(data.posts);
+
+      setFetching(false);
     })
     
   } , [])
 
-
-
-
   return (
     <div>
-      {postData.length === 0 && <WelcomeMessage/>} 
-      {postData.map( item => <Post item = {item} key={item.id}/>
+      {fetching && <LoadingSpinner/>}
+      {!fetching && postData.length === 0 && <WelcomeMessage/>} 
+      {!fetching && postData.map( item => <Post item = {item} key={item.id}/>
         // {console.log(data)} 
         )
       } 
